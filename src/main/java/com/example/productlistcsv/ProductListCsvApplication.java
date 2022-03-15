7d2e7d3521235117052ad3648efaf640b020de7b
vlsolodilov <vlsolodilov@gmail.com>
2022-03-15 11:12:10 +0300

    init

---
+++ src/main/java/com/example/productlistcsv/ProductListCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.productlistcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListCsvApplication.class, args);
+	}
+
+}
